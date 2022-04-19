37b625dca7a44045bd7ab6da0d1fbd939b739aae
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-04-19 19:56:52 -0400

    completed accordion

--- hooks/react-hooks/src/components/Accordian.js
+++ hooks/react-hooks/src/components/Accordian.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordian = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-const onTitleClick = (index) => {
-    console.log('title clicked! ',index)
-}
-
-  const renderItems = items.map((item, i) => {
-    return (
-      <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(i)}>
-          <i className="dropdown icon"></i>
-          {item.title}
-        </div>
-        <div className="content active">
-          <p>{item.content}</p>
-        </div>
-      </React.Fragment>
-    );
-  });
-
-  return <div className="ui styled accordion">{renderItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {items.map((item, i) => {
+        return (
+          <React.Fragment key={item.title}>
+            <div
+              className={`title ${i === activeIndex ? "active" : ""}`}
+              onClick={() => setActiveIndex(i)}
+            >
+              <i className="dropdown icon"></i>
+              {item.title}
+            </div>
+            <div className={`content  ${i === activeIndex ? "active" : ""}`}>
+              <p>{item.content}</p>
+            </div>
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Accordian;
