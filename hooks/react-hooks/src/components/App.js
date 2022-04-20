b72ba9778ab8004978c2175d29be603620710d13
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-04-19 21:47:10 -0400

    added search, working on input delay for api call

---
+++ hooks/react-hooks/src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Search from "./Search";
+
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front-end javascrip library",
+//   },
+//   {
+//     title: "What use React?",
+//     content: "React is a favorite JS library among  engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use react by creating components",
+//   },
+// ];
+
+
+const App = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default App;
