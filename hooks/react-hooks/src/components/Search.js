a8a1c257416dee1db8f934a3cd9317ab922d1e56
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-04-21 08:07:29 -0400

    fixed search and added debounce

--- hooks/react-hooks/src/components/Search.js
+++ hooks/react-hooks/src/components/Search.js
@@ -3,39 +3,53 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
-      const {data} = await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-    if(term) search();
-  }, [term])
-  
-  const renderedResults = results.map(result => {
-      return (
-          <div key={result.pageid} className="item">
-              <div className="right floated content">
-                  <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
-              </div>
-              <div className="content">
-                  <div className="header">
-                      {result.title}
-                  </div>
-                  <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
-              </div>
-          </div>
-      )
-  })
+
+    search();
+  }, [debouncedTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -50,9 +64,7 @@ const Search = () => {
           />
         </div>
       </div>
-      <div className="ui celled list">
-          {renderedResults}
-      </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
