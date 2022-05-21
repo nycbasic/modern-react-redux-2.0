b489f19359e4c46636d88bed8fb2b1cbe5dc0daf
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-05-21 17:35:30 -0400

    changed searchBar component from class to function with hooks

--- hooks/youtube-videos-hooks/src/components/SearchBar.js
+++ hooks/youtube-videos-hooks/src/components/SearchBar.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: '' };
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  onInputChange = event => {
-    this.setState({ term: event.target.value });
+  const onInputChange = (e) => {
+    setTerm(e.target.value);
   };
 
-  onFormSubmit = event => {
-    event.preventDefault();
-
-    this.props.onFormSubmit(this.state.term);
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    return onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={handleSubmitForm} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input type="text" value={term} onChange={onInputChange} />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
