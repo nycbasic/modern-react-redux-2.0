59677244a398a68df09ee56d996a3ced3ffca867
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-05-31 07:35:20 -0400

    setting up redux for async and data fetching

--- redux/blog-async-react-redux/src/index.js
+++ redux/blog-async-react-redux/src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 import reportWebVitals from "./reportWebVitals";
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
